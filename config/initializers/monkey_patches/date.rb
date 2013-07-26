f0fbab1d5b58f7d8be8c5c193a4f95a9322c4a10
stevenbristol <stevenbristol@gmail.com>
2013-07-26 11:11:53 -0400

    fixing date bug

--- config/initializers/monkey_patches/date.rb
+++ config/initializers/monkey_patches/date.rb
@@ -1,10 +1,10 @@
 class Date
   
-  def week
+  def self.week
     Date.current.beginning_of_week..Date.current
   end
   
-  def month
+  def self.month
     Time.current.beginning_of_month.to_date..Date.current
   end
   
