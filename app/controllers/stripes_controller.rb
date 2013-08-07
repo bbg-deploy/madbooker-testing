c40bf4007f4ef3df5b86a01dbe0788632af8ed9f
stevenbristol <stevenbristol@gmail.com>
2013-08-07 11:48:22 -0400

    stripe controller

--- app/controllers/stripes_controller.rb
+++ app/controllers/stripes_controller.rb
@@ -4,6 +4,7 @@ class StripesController < ApplicationController
   skip_filter :set_time_zone
   skip_filter :set_reservation_cookie
   skip_filter :store_page_stat
+  skip_before_action :verify_authenticity_token
   
   respond_to :html, :js, :json, :xml
   
