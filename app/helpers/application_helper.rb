a6511fc0b9720ca357d8a5d2595dadae524344f3
Allan Branch <allan@lesseverything.com>
2013-08-06 19:48:53 -0500

    navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,9 @@ module ApplicationHelper
   
   def hotel_link
     return link_to( "Create your hotel", new_hotel_path) if current_hotel.blank?
-    link_to "Dashboard", current_hotel
+    link_to current_hotel do
+      "<i class='icon-dashboard'></i> Dashboard".html_safe
+      end
   end
   
   def replace_with_partial(dom_el, partial)
