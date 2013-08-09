55450a138a2158b3ecc4669afe21c68a10498308
stevenbristol <stevenbristol@gmail.com>
2013-08-09 15:27:10 -0400

    deashboard

--- app/decorators/dashboard_decorator.rb
+++ app/decorators/dashboard_decorator.rb
@@ -1,18 +1,28 @@
 class DashboardDecorator < HotelDecorator
   
   
+  def mobile_revenue_this_week
+    
+  end
+  
+  def tablet_revenue_this_week
+    
+  end
+  
+  def other_revenue_this_week
+    
+  end
   
   def revenue_this_week
-    o = mobile_other_total_from_group model.sales.range(Time.week).group(:mobile).paid.sum( :price)
-    #link_to o, [:searches, current_hotel, :reports]
+    #o = mobile_other_total_from_group model.sales.range(Time.week).group(:mobile).paid.sum( :price)
   end
   
   def revenue_this_month
-    mobile_other_total_from_group model.sales.range(Time.month).group(:mobile).paid.sum( :price)
+    #mobile_other_total_from_group model.sales.range(Time.month).group(:mobile).paid.sum( :price)
   end
   
   def revenue_daily_average
-    mobile_other_total_from_group model.sales.range(Time.month).group(:mobile).paid.average( :price)
+    #mobile_other_total_from_group model.sales.range(Time.month).group(:mobile).paid.average( :price)
   end
   
   def revenue_by_room_type
@@ -20,30 +30,30 @@ class DashboardDecorator < HotelDecorator
   end
   
   def searches_this_week
-    o = mobile_other_total_from_group model.stats.searches.range(Time.week).group(:mobile).count
-    link_to o, searches_hotel_reports_path(current_hotel, date_range: :month)
+    #o = mobile_other_total_from_group model.stats.searches.range(Time.week).group(:mobile).count
+    #link_to o, searches_hotel_reports_path(current_hotel, date_range: :month)
   end
   
   def searches_this_month
-    mobile_other_total_from_group searches_this_month_raw
+    #mobile_other_total_from_group searches_this_month_raw
   end
   
   def searches_daily_average
     x = searches_this_month_raw
-    mobile_other_total x[true].to_d/ Date.current.day, x[false].to_d/ Date.current.day
+    #mobile_other_total x[true].to_d/ Date.current.day, x[false].to_d/ Date.current.day
   end
   
   def look_to_book_week
-    mobile_other_average *look_to_book_ratio( model.stats.range(Time.week).group(:kind, :mobile).look_to_book.count)
+    #mobile_other_average *look_to_book_ratio( model.stats.range(Time.week).group(:kind, :mobile).look_to_book.count)
   end
   
   def look_to_book_month
-    mobile_other_average *look_to_book_ratio( model.stats.range(Time.month).group(:kind, :mobile).look_to_book.count)    
+    #mobile_other_average *look_to_book_ratio( model.stats.range(Time.month).group(:kind, :mobile).look_to_book.count)    
   end
   
   def look_to_book_daily_average
-    m, o  = look_to_book_ratio( model.stats.range(Time.month).group(:kind, :mobile).look_to_book.count)    
-    mobile_other_average m/Date.current.day, o/Date.current.day    
+    #m, o  = look_to_book_ratio( model.stats.range(Time.month).group(:kind, :mobile).look_to_book.count)    
+    #mobile_other_average m/Date.current.day, o/Date.current.day    
   end
   
   
