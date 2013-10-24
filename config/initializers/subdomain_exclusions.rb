a1e4c170f2cc58b9428614fd2ed25459090af71a
stevenbristol <stevenbristol@gmail.com>
2013-10-24 08:45:23 -0400

    updating subdomain exclusions

--- config/initializers/subdomain_exclusions.rb
+++ config/initializers/subdomain_exclusions.rb
@@ -1,3 +1,3 @@
 
-SUBDOMAIN_EXCLUSIONS = %w{www mail test ftp admin alpha beta pop pop3 server1 webmail smtp mx blog app}
+SUBDOMAIN_EXCLUSIONS = %w{www mail test ftp admin alpha beta pop pop3 server1 webmail smtp mx blog app calendar docs sites www}
 
