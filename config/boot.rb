48109676a31af2a4e9b1a87969380b14325f1648
Tripp Waldrop <twaldrop@bluebox.net>
2014-01-09 11:17:27 -0800

    add BUNDLE_GEMFILE path to boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,6 @@
 # Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+current_path = "/srv/apps/madbooker/current"
+ENV['BUNDLE_GEMFILE'] = "#{current_path}/Gemfile"
+#ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
